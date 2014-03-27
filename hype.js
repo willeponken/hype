42bdb40364fcd871753b175170929bcae2b25a0e
willeponken <willeponken@gmail.com>
2014-03-27 23:00:23 +0100

    Smexy colors is nice

--- hype.js
+++ hype.js
@@ -4,22 +4,28 @@ var http = require('http')
   /* User specific variables
    * If the port is lower than or equal to 1024; run as root. */
   , NAME = 'Hype'
-  , PORT = 81
+  , PORT = 80
   , ADDRESS = 'fc74:73e8:3913:f15b:d463:2fe7:db69:381e';
 
 function onRequest(req, res) {
   console.log(req.method, req.url);
-  console.log(this.name + ': got request.');
+  console.log(this.name + ': Request from ' + req.connection.remoteAddress);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('such cjdns\n\n                       such illegal\n\n      such hype\n\n           such ipv6 ' + req.connection.remoteAddress);
 }
 
 function onListening() {
-  console.log(this.name + ': Listening on ' + ADDRESS + ':' + PORT);
+  console.log(this.name + ': Listening on http://[' + ADDRESS + ']:' + PORT + '/');
   console.log(this.name + ': Press CTRL+C to terminate');
 }
 
-server.name = NAME;
-server.on('request', onRequest);
-server.on('listening', onListening);
-server.listen(PORT, ADDRESS);
+function startServer(name, address, port) {
+  server.name = '\x1B[34m' + name + '\x1B[00m';
+  server.on('request', onRequest);
+  server.on('listening', onListening);
+  server.listen(port, address);
+  return server;
+}
+
+// Initialize and listen
+startServer(NAME, ADDRESS, PORT);
